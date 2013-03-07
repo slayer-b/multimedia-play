a3246c0c9190acce582ed8194da40dd3c740d903
slayer <demchuck.dima@gmail.com>
2013-03-07 20:54:50 +0200

    Added build number to appVersion.

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import play.Project._
 object ApplicationBuild extends Build {
 
   val appName         = "multimedia-play"
-  val appVersion      = "1.0-SNAPSHOT"
+  val appVersion      = "1.0-" + System.getProperty("build.number")
 
   val appDependencies = Seq(
     "mysql" % "mysql-connector-java" % "5.1.18",
