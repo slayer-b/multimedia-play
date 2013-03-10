b1758f6d3e15e24047085512f4f7e2b551104193
slayer <demchuck.dima@gmail.com>
2013-03-10 22:41:23 +0200

    EditWallpaperPublicSpec

---
+++ test/EditWallpaperPublicSpec.scala
@@ -0,0 +1,50 @@
+package test
+
+import org.specs2.mutable._
+
+import play.api.test._
+import play.api.test.Helpers._
+import play.api.libs.json.Json
+import org.codehaus.jackson.annotate.JsonValue
+import play.api.Logger
+import akka.testkit.TestKit
+
+class EditWallpaperPublicSpec extends Specification {
+  val id = "1"
+  val field = "description"
+
+  "EditWallpaperPublic" should {
+
+    "add new record into cache" in {
+      running(FakeApplication()) {
+        val result = addRecord("test-1")
+
+        status(result) must equalTo(OK)
+        contentAsString(result) must equalTo("{\"status\":\"OK\"}")
+      }
+    }
+
+    "get records from cache" in {
+      running(FakeApplication()) {
+        addRecord("test-1")
+        addRecord("test-2")
+
+        val request = FakeRequest(POST, "/p/c/w/view_cache.json").withJsonBody(Json.toJson(""))
+        val result = route(request).get
+
+        status(result) must equalTo(OK)
+        contentAsString(result) must equalTo("[{\"id\":\"1\",\"entry_id\":1,\"field\":\"description\",\"value\":\"test-2\"}," +
+          "{\"id\":\"1\",\"entry_id\":0,\"field\":\"description\",\"value\":\"test-1\"}]")
+      }
+    }
+
+  }
+
+  def addRecord(value: String) = {
+    val request = FakeRequest(POST, "/p/c/w/edit").withJsonBody(
+      Json.toJson(
+        Map("id" -> id, "field" -> field, "value" -> value)
+      ))
+    route(request).get
+  }
+}
